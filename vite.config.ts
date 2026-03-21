c1419d9981f2c729a1a1c5d716bafe2f70240cf6
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2026-03-21 09:53:37 +0000

    tailwindを使用する設定に変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     base: '/conlang-tool',
     build: {
         outDir: "conlang-tool"
     },
-    plugins: [solid()],
+    plugins: [
+        tailwindcss(),
+        solid()
+    ],
 });
